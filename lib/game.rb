69e0f11dcaf8293da9c4107490d24c049ecccfde
byverdu <byverdu@gmail.com>
2014-08-27 12:21:47 +0100

    progressed the ask placement method

--- lib/game.rb
+++ lib/game.rb
@@ -11,5 +11,17 @@ class Game
 	@board2 = Board.new(player2)
 	end
 
-	
+	def ask_placement(board)
+		board.ships.each do |ship|
+			length = ship.length
+			@placement = []
+			while length > 0 do
+				puts "please input placement cell for #{ship}, ie:B11"
+				coordinates = gets.chomp.capitalize
+				@placement << coordinates
+				length-=1
+			end
+		end
+		@placement
+	end
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,9 @@ describe Game do
 
 	let(:player1) { double :player  	}
 	let(:player2) { double :player  	}
-	let(:board1)		{ double :board 		}
+	let(:board1)		{ double :board 	}
 	let(:game)		{ Game.new					}
+	let(:ship) { double :ship }
 
 	it "should initialize 2 players" do		
 		expect(game.player1).to be_a(Player) 
@@ -15,7 +16,9 @@ describe Game do
 		expect(game.board1).to be_a(Board) 
 	end
 
-	xit 'asks the player to place the ships' do
+	it 'asks the player to place the ships' do
+	expect(ship).to receive(:length).and_return(3)
+	expect(board1).to receive(:ships).and_return([ship])
 	expect(game.ask_placement(board1)).to match([/[A-Z]\d\d/,/[A-Z]\d\d/]) 
 	end
 
